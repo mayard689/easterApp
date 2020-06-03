61df2a5d29a3d64c26423246b6bc1d9ae8e1242c
benoitchocot <benoit.chocot@gmail.com>
2020-06-03 17:39:11 +0200

    added h1 and table

---
+++ src/Controller/FeaturesController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FeaturesController extends AbstractController
+{
+    /**
+     * @Route("/features", name="features")
+     */
+    public function index()
+    {
+        return $this->render('features/index.html.twig', [
+            'controller_name' => 'FeaturesController',
+            'title' => 'Fonctionnalités récurrentes'
+        ]);
+    }
+}
