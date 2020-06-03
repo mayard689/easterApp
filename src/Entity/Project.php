3f1d8224a4b756c618650ac8944edea860379f45
mayard689 <doc-albert@laposte.net>
2020-06-03 23:19:26 +0200

    project entity and repository created (without foreign key to application and level, no reverse key to project_feature)

---
+++ src/Entity/Project.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProjectRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProjectRepository::class)
+ */
+class Project
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $expert;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $confirmed;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $junior;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getExpert(): ?int
+    {
+        return $this->expert;
+    }
+
+    public function setExpert(int $expert): self
+    {
+        $this->expert = $expert;
+
+        return $this;
+    }
+
+    public function getConfirmed(): ?int
+    {
+        return $this->confirmed;
+    }
+
+    public function setConfirmed(int $confirmed): self
+    {
+        $this->confirmed = $confirmed;
+
+        return $this;
+    }
+
+    public function getJunior(): ?int
+    {
+        return $this->junior;
+    }
+
+    public function setJunior(int $junior): self
+    {
+        $this->junior = $junior;
+
+        return $this;
+    }
+}
