8cda18a07567b360f9ad985528da1a45f45b1f9f
benoitchocot <benoit.chocot@gmail.com>
2020-06-04 11:30:14 +0200

    add feature.name field, add db fields

--- src/Controller/FeaturesController.php
+++ src/Controller/FeaturesController.php
@@ -2,19 +2,32 @@
 
 namespace App\Controller;
 
+use App\Entity\Feature;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FeaturesController extends AbstractController
 {
     /**
      * @Route("/features", name="features")
+     * @return Response
      */
-    public function index()
+    public function index(): Response
     {
+        $features = $this->getDoctrine()
+            ->getRepository(Feature::class)
+            ->findAll();
+
+        if (!$features) {
+            throw $this->createNotFoundException(
+                'No features found in program\'s table.'
+            );
+        }
         return $this->render('features/index.html.twig', [
             'controller_name' => 'FeaturesController',
-            'title' => 'Liste des Fonctionnalités Récurrentes'
+            'title' => 'Liste des Fonctionnalités Récurrentes',
+            'features' => $features,
         ]);
     }
 }
