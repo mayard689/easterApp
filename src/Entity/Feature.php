38603bcce03464dcd4fec945c7c7a15ebd80e9d6
benoitchocot <benoit.chocot@gmail.com>
2020-06-04 10:58:07 +0200

    add entities

--- src/Entity/Feature.php
+++ src/Entity/Feature.php
@@ -17,8 +17,59 @@ class Feature
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $day;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDay(): ?float
+    {
+        return $this->day;
+    }
+
+    public function setDay(float $day): self
+    {
+        $this->day = $day;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
     }
 }
