2efcb14e1b2019d136955fe5285cc11fe314d0c1
dante18 <lablandinerie2@gmail.com>
2020-06-28 11:03:57 +0200

    Verifications of the values entered in the fields of the form

--- src/Form/ChangePasswordType.php
+++ src/Form/ChangePasswordType.php
@@ -8,18 +8,39 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class ChangePasswordType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('plainPassword', RepeatedType::class, array(
+            ->add('password', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'first_options' => array('label' => 'Nouveau mot de passe'),
                 'second_options' => array('label' => 'Confirmer le mot de passe'),
                 'invalid_message' => 'Les 2 mots de passe ne sont pas identiques.',
-            ))
+                'constraints' => [
+                    new NotBlank(
+                        ['message' => 'Merci de saisir un mot de passe']
+                    ),
+                    new Length(
+                        [
+                            'min' => 8,
+                            'minMessage' => 'Le mot de passe doit contenir minimum 8 caractères'
+                        ]
+                    ),
+                    new Regex(
+                        [
+                            'pattern' => '^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?\W).*$',
+                            'message' => 'Le mot de passe doit contenir au minimum 1
+                                chiffre, 1 majuscule, et un caractère spécial.'
+                        ]
+                    )
+                ]
+            ])
         ;
     }
 
