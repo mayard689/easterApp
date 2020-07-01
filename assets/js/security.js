0557c93121b6153b42fcf1c790b9a70fb3b4a5c8
dante18 <lablandinerie2@gmail.com>
2020-07-01 14:36:59 +0200

    Layout of the login page

--- assets/js/security.js
+++ assets/js/security.js
@@ -3,17 +3,29 @@ const button = document.querySelectorAll('.button_password');
 // eslint-disable-next-line no-plusplus
 for (let i = 0; i < button.length; i++) {
     button[i].addEventListener('click', () => {
-        const input = document.getElementsByClassName('form-control');
+        let input = document.getElementsByClassName('form-control');
         const buttonIcon = document.getElementsByClassName('fas');
 
         if (buttonIcon[i].classList[1] === 'fa-eye') {
             buttonIcon[i].classList.remove('fa-eye');
             buttonIcon[i].classList.add('fa-eye-slash');
-            input[i].setAttribute('type', 'text');
+
+            if (document.getElementById('inputPassword') != null) {
+                input = document.getElementById('inputPassword');
+                input.setAttribute('type', 'text');
+            } else {
+                input[i].setAttribute('type', 'text');
+            }
         } else {
             buttonIcon[i].classList.remove('fa-eye-slash');
             buttonIcon[i].classList.add('fa-eye');
-            input[i].setAttribute('type', 'password');
+
+            if (document.getElementById('inputPassword') != null) {
+                input = document.getElementById('inputPassword');
+                input.setAttribute('type', 'password');
+            } else {
+                input[i].setAttribute('type', 'password');
+            }
         }
     });
 }
