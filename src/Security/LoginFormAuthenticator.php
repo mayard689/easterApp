c4bfc20ba17d50f82a01fb4aeb7a515beec1504d
dante18 <lablandinerie2@gmail.com>
2020-07-01 15:05:03 +0200

    Adding a link to log out and managing access to the users page

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -100,7 +100,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
             return new RedirectResponse($this->getTargetPath($request->getSession(), $providerKey));
         }
 
-        return new RedirectResponse($this->urlGenerator->generate('home'));
+        return new RedirectResponse($this->urlGenerator->generate('home_index'));
     }
 
     protected function getLoginUrl()
