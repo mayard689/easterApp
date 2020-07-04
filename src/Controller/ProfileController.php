5e80965816a0a2f4335d943d458613e418f2dcb4
dante18 <lablandinerie2@gmail.com>
2020-07-04 20:16:59 +0200

    Formatting a user's profile page

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -3,17 +3,21 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class ProfileController extends AbstractController
 {
     /**
      * @Route("/profile", name="profile")
+     * @param UserInterface $user
+     * @return Response
      */
-    public function index()
+    public function index(UserInterface $user): Response
     {
         return $this->render('security/profile.html.twig', [
-            'controller_name' => 'ProfileController',
+            'user' => $user
         ]);
     }
 }
