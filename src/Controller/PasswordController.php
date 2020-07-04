9c59522b2ef51cc55e6e3f9ad34bce1b2dec2128
dante18 <lablandinerie2@gmail.com>
2020-07-04 15:19:53 +0200

    Processing the password reset request

--- src/Controller/PasswordController.php
+++ src/Controller/PasswordController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\ChangePasswordType;
 use App\Form\RequestResetPassword;
+use App\Form\ResetPasswordType;
 use App\Service\MailManager;
 use DateTime;
 use Exception;
@@ -17,19 +18,89 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
 
 /**
- * @Route("/changePassword")
+ * @Route("/password")
  */
-class ChangePasswordController extends AbstractController
+class PasswordController extends AbstractController
 {
     /**
-     * @Route("/{id}/{token}", name="changePassword_index")
+     * @Route("/forgot", name="password_forgot_request")
+     * @param Request $request
+     * @param MailManager $mailManager
+     * @param TokenGeneratorInterface $tokenGenerator
+     * @return Response
+     * @throws Exception
+     */
+    public function forgotPasswordRequest(
+        Request $request,
+        MailManager $mailManager,
+        TokenGeneratorInterface $tokenGenerator
+    ): Response {
+        $form = $this->createForm(RequestResetPassword::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $user = $entityManager->getRepository(User::class)->findOneBy(
+                [
+                    'email' => $form->getData()['email']
+                ]
+            );
+
+            if ($user instanceof User) {
+                $this->addFlash('success', 'Votre demande à bien été pris en compte. Un mail va vous
+                être envoyé afin que vous puissiez renouveller votre mot de passe. Le lien que vous
+                recevrez sera valide 3h.');
+
+                $date = new DateTime();
+                $token = $tokenGenerator->generateToken();
+                $user->setCreationDate($date);
+                $user->setToken($tokenGenerator->generateToken());
+                $user->setPasswordRequestedAt($date);
+                $user->setToken($token);
+
+                $entityManager->persist($user);
+                $entityManager->flush();
+
+                $sendParameter = [
+                    'from' => 'no-reply <' . $this->getParameter('mailer_from') . '>',
+                    'to' => $user->getEmail(),
+                    'subject' => 'Renouvellement du mot de passe'
+                ];
+
+                $bodyData = [
+                    'title' => 'Bonjour',
+                    'bodyText' => 'Pour modifier votre mot de passe, vous pouvez cliquer sur le bouton ci-dessous.',
+                    'pageLink' => 'password_reset',
+                    'buttonName' => 'Saisir mon mot de passe',
+                    'userId' => $user->getId(),
+                    'userToken' => $token
+                ];
+
+                $mailManager->sendMessage($sendParameter, 'user/notification/notification.html.twig', $bodyData);
+
+                $this->redirectToRoute('app_login');
+            } else {
+                $this->addFlash('danger', 'L\'adresse email n\'existe pas, vérifiez l\'adresse 
+                que vous avez renseignée.');
+
+                $this->redirectToRoute('password_forgot_request');
+            }
+        }
+
+        return $this->render('security/requestpassword.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/{token}", name="password_add")
      * @param User $user
      * @param mixed $token
      * @param Request $request
      * @param UserPasswordEncoderInterface $passwordEncoder
      * @return Response
      */
-    public function index(User $user, $token, Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    public function add(User $user, $token, Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
     {
         /*
             We prohibit access to the page if the token is null
@@ -64,88 +135,69 @@ class ChangePasswordController extends AbstractController
             return $this->redirectToRoute('project_index');
         }
 
-        return $this->render('security/changepassword.html.twig', [
+        return $this->render('password.html.twig', [
             'form' => $form->createView()
         ]);
     }
 
     /**
-     * @Route("/resetPasswordRequest", name="resetPasswordRequest")
+     * @Route("{/id}/reset/{token}", name="password_reset")
+     * @param User $user
+     * @param TokenGeneratorInterface $token
      * @param Request $request
-     * @param MailManager $mailManager
-     * @param TokenGeneratorInterface $tokenGenerator
+     * @param UserPasswordEncoderInterface $passwordEncoder
      * @return Response
-     * @throws Exception
      */
-    public function resetPasswordRequest(
+    public function reset(
+        User $user,
+        $token,
         Request $request,
-        MailManager $mailManager,
-        TokenGeneratorInterface $tokenGenerator
+        UserPasswordEncoderInterface $passwordEncoder
     ): Response {
-        $form = $this->createForm(RequestResetPassword::class);
+        /*
+            We prohibit access to the page if the token is null
+            or if it has been created for more than 3 hours
+        */
+        if (!$this->isRequestInTime(180, $user->getPasswordRequestedAt())
+            || $this->checkToken($token, $user->getToken() == false)
+        ) {
+            throw new AccessDeniedHttpException();
+        }
+
+        $form = $this->createForm(ResetPasswordType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $password = $passwordEncoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($password);
+
+            // reset the token to null so that it is no longer reusable
+            $user->setToken(null);
+            $user->setPasswordRequestedAt(null);
+
             $entityManager = $this->getDoctrine()->getManager();
-            $user = $entityManager->getRepository(User::class)->findOneBy(
-                [
-                    'email' => $form->getData()['email']
-                ]
-            );
+            $entityManager->persist($user);
+            $entityManager->flush();
 
-            if ($user instanceof User) {
-                $this->addFlash('success', 'Votre demande à bien été pris en compte. Un mail va vous
-                être envoyé afin que vous puissiez renouveller votre mot de passe. Le lien que vous
-                recevrez sera valide 24h.');
+            $this->addFlash('success', 'Votre mot de passe a été réinitialiser.');
 
-                $date = new DateTime();
-                $token = $tokenGenerator->generateToken();
-                $user->setCreationDate($date);
-                $user->setToken($tokenGenerator->generateToken());
-                $user->setPasswordRequestedAt($date);
-                $user->setToken($token);
-
-                $entityManager->persist($user);
-                $entityManager->flush();
-
-                $sendParameter = [
-                    'from' => 'no-reply <' . $this->getParameter('mailer_from') . '>',
-                    'to' => $user->getEmail(),
-                    'subject' => 'Renouvellement du mot de passe'
-                ];
-
-                $bodyData = [
-                    'title' => 'Bonjour',
-                    'bodyText' => 'Pour modifier votre mot de passe, vous pouvez cliquer sur le bouton ci-dessous.',
-                    'pageLink' => 'changePassword_index',
-                    'buttonName' => 'Saisir mon mot de passe',
-                    'userId' => $user->getId(),
-                    'userToken' => $token
-                ];
-
-                $mailManager->sendMessage($sendParameter, 'user/notification/notification.html.twig', $bodyData);
-
-                $this->redirectToRoute('resetPasswordRequest');
-            } else {
-                $this->addFlash('danger', 'L\'adresse email n\'existe pas, vérifiez l\'adresse 
-                que vous avez renseignée.');
-
-                $this->redirectToRoute('resetPasswordRequest');
-            }
+            return $this->redirectToRoute('project_index');
         }
 
-        return $this->render('security/requestpassword.html.twig', [
-            'form' => $form->createView()
+        return $this->render('security/password.html.twig', [
+            'form' => $form->createView(),
+            'title' => 'Modifier votre mot de passe'
         ]);
     }
 
     /**
-     * Check that the token was created less than 10 minutes ago
+     * Check that the token is still valid
      *
      * @param mixed $passwordRequestedAt
+     * @param mixed $tokenTimeValid
      * @return bool
      */
-    private function isRequestInTime($passwordRequestedAt = null)
+    private function isRequestInTime($tokenTimeValid, $passwordRequestedAt = null)
     {
         if ($passwordRequestedAt == null) {
             return false;
@@ -153,10 +205,15 @@ class ChangePasswordController extends AbstractController
 
         $now = new DateTime();
         $interval = $now->getTimestamp() - $passwordRequestedAt->getTimestamp();
-        $daySeconds = 60 * 10;
+        $daySeconds = 60 * $tokenTimeValid;
 
         return $interval > $daySeconds ? false : true;
     }
 
-    public function request(){}
+    private function checkToken($token, $userToken): bool
+    {
+        if (is_null($userToken) || $token !== $userToken) {
+            return false;
+        }
+    }
 }
---
+++ src/Form/ResetPasswordType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ResetPasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => array('label' => 'Nouveau mot de passe'),
+                'second_options' => array('label' => 'Confirmer le mot de passe'),
+                'invalid_message' => 'Les 2 mots de passe ne sont pas identiques.',
+                'help' => 'Le mot de passe doit contenir 8 caractères, dont 1
+                            chiffre, 1 majuscule, et un caractère spécial.'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+            'validation_groups' => ['Password']
+        ]);
+    }
+}
