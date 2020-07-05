59485a06c3e0ca42a292ae7420818ff73ee7e255
dante18 <lablandinerie2@gmail.com>
2020-07-05 14:06:35 +0200

    Processing of the form of the 'Informations Personnel' block

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -7,6 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class ProfileController extends AbstractController
@@ -21,6 +22,17 @@ class ProfileController extends AbstractController
     {
         $form = $this->createForm(UserUpdateType::class, $user);
         $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Votre profil a bien été mis à jour.');
+
+            return $this->redirectToRoute('profile');
+        }
+
 
         return $this->render('security/profile.html.twig', [
             'user' => $user,
