5e272afc9efb5aec03d4c27bf07521ebf35e9ec2
DIERICK Dewi <dierickd@outlook.fr>
2020-07-06 17:03:19 +0200

    added constraint unique entity

--- src/Entity/Feature.php
+++ src/Entity/Feature.php
@@ -6,10 +6,12 @@ use App\Repository\FeatureRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=FeatureRepository::class)
+ * @UniqueEntity("name", message="Le nom de la fonctionnalité existe déjà")
  */
 class Feature
 {
