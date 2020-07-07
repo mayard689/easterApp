09e95c299f4eed468d96e604b16558b1913add4a
dante18 <lablandinerie2@gmail.com>
2020-07-07 17:43:10 +0200

    Add form to upload profile photo

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\AvatarUpdateType;
 use App\Form\ChangePasswordType;
 use App\Form\UserUpdateType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -25,7 +26,10 @@ class ProfileController extends AbstractController
         $user = $this->getUser();
 
         $form = $this->createForm(UserUpdateType::class, $user);
+        $formUpdateAvatar = $this->createForm(AvatarUpdateType::class, $user);
+
         $form->handleRequest($request);
+        $formUpdateAvatar->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager = $this->getDoctrine()->getManager();
@@ -37,10 +41,21 @@ class ProfileController extends AbstractController
             return $this->redirectToRoute('profile_index');
         }
 
+        if ($formUpdateAvatar->isSubmitted() && $formUpdateAvatar->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Votre photo de profil a bien été mise à jour.');
+
+            return $this->redirectToRoute('profile_index');
+        }
+
 
         return $this->render('security/profile.html.twig', [
             'user' => $user,
             'form' => $form->createView(),
+            'formUpdateAvatar' => $formUpdateAvatar->createView()
         ]);
     }
 
---
+++ src/Form/AvatarUpdateType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+
+class AvatarUpdateType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('profilePictureFile', VichImageType::class, [
+                'help'         =>
+                    'Les fichiers autorisés sont uniquement de type '
+                    . implode(', ', User::MIME_TYPES)
+                    . ' et le poids maximal de ' . User::MAX_SIZE,
+                'required'     => false,
+                'download_uri' => false,
+                'allow_delete' => false,
+                'image_uri' => false,
+                'label' => false
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+            'validation_groups' => ['AvatarUpdate']
+        ]);
+    }
+}
