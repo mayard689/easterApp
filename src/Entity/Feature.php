eadd6c2c1ea5754891456432c6ffc7c6de855a61
DIERICK Dewi <dierickd@outlook.fr>
2020-07-08 15:12:03 +0200

    resolved error formFeature

--- src/Entity/Feature.php
+++ src/Entity/Feature.php
@@ -11,7 +11,6 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=FeatureRepository::class)
- * @UniqueEntity("name", message="La fonctionnalité {{ value }} existe déjà")
  */
 class Feature
 {
