3ff1fd103ed869b5afa0d7d2b2d9d47243e1e31e
benoitchocot <benoit.chocot@gmail.com>
2020-07-09 14:55:57 +0200

    fix by variant

--- src/Controller/ExportController.php
+++ src/Controller/ExportController.php
@@ -4,11 +4,13 @@ namespace App\Controller;
 
 use App\Entity\Project;
 use App\Repository\ProjectFeatureRepository;
+use App\Repository\ProjectRepository;
 use App\Repository\QuotationRepository;
 use App\Service\ProjectCalculator;
 use Dompdf\Dompdf;
 use Dompdf\Options;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,7 +20,7 @@ class ExportController extends AbstractController
      * @Route("/{id}/export/{variant<high|middle|low>}", name="export")
      * @param Project $project
      * @param ProjectCalculator $projectCalculator
-     * @param QuotationRepository $quotationRepository
+     * @param ProjectRepository $projectRepository
      * @param string $variant
      * @return Response
      */
@@ -26,9 +28,13 @@ class ExportController extends AbstractController
     public function index(
         Project $project,
         ProjectCalculator $projectCalculator,
-        QuotationRepository $quotationRepository,
+        ProjectRepository $projectRepository,
         string $variant = 'high'
     ): Response {
+
+
+        $featureCategories = $projectRepository->getCategories($project);
+
         // Configure Dompdf according to your needs
         $pdfOptions = new Options();
         $pdfOptions->set('defaultFont', 'Arial');
@@ -42,7 +48,8 @@ class ExportController extends AbstractController
             'project' => $project,
             'costs' => $projectCalculator->calculateProjectsFigures(),
             'variant' => $variant,
-            'variants' => $quotationRepository->findAll(),
+            'featureCategories' => $featureCategories,
+
 
         ]);
 
