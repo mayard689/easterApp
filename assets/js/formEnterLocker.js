875f55def1c32e9ef0923ef6b962e33458f36e81
mayard689 <doc-albert@laposte.net>
2020-07-20 13:03:55 +0200

    use standard wrtiting rules

--- assets/js/formEnterLocker.js
+++ assets/js/formEnterLocker.js
@@ -1,5 +1,7 @@
 
+// eslint-disable-next-line no-undef
 $(document).ready(() => {
+    // eslint-disable-next-line no-undef
     $(window).keydown((event) => {
         if (event.keyCode === 13) {
             event.preventDefault();
